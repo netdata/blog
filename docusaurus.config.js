7cada5385e6c7167bb5c7d5ca693032aa278efb6
Panos Christophides <panos@netdata.cloud>
2022-11-04 17:17:01 +0200

    google search styling

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,58 +1,63 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/palenight');
-const darkCodeTheme = require('prism-react-renderer/themes/vsDark');
+const lightCodeTheme = require("prism-react-renderer/themes/palenight");
+const darkCodeTheme = require("prism-react-renderer/themes/vsDark");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Netdata Blog',
-  tagline: 'Home of the Netdata blog.',
-  url: 'https://blog.netdata.cloud',
-  baseUrl: '/',
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon-32x32.png',
+  title: "Netdata Blog",
+  tagline: "Home of the Netdata blog.",
+  url: "https://blog.netdata.cloud",
+  baseUrl: "/",
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon-32x32.png",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
-  themes: [
-  ],
+  themes: [],
 
   scripts: [
-    {src:'https://cse.google.com/cse.js?cx=e2e7646659949450a', async: false, defer: false},
+    {
+      src: "https://cse.google.com/cse.js?cx=e2e7646659949450a",
+      async: true,
+      defer: false,
+    },
   ],
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: false,
         blog: {
-          routeBasePath: '/', 
+          routeBasePath: "/",
           showReadingTime: true,
-          blogTitle: 'Netdata Blog',
-          blogDescription: 'Home of the Netdata blog about monitoring and troubleshooting.',
-          blogSidebarTitle: 'Blog Posts',
-          blogSidebarCount: 'ALL',
-          postsPerPage: 'ALL',
+          blogTitle: "Netdata Blog",
+          blogDescription:
+            "Home of the Netdata blog about monitoring and troubleshooting.",
+          blogSidebarTitle: "Blog Posts",
+          blogSidebarCount: "ALL",
+          postsPerPage: "ALL",
           feedOptions: {
-            type: 'all',
-            title: 'Netdata Blog',
-            description: 'Home of the Netdata blog about monitoring and troubleshooting.',
-            language: 'en',
+            type: "all",
+            title: "Netdata Blog",
+            description:
+              "Home of the Netdata blog about monitoring and troubleshooting.",
+            language: "en",
             copyright: `Copyright © ${new Date().getFullYear()} Netdata`,
           },
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -60,40 +65,40 @@ const config = {
 
   stylesheets: [
     {
-        href: '/font/ibm-plex-sans-v8-latin-regular.woff2',
-        rel: 'preload',
-        as: 'font',
-        type: 'font/woff2',
-        crossorigin: '',
+      href: "/font/ibm-plex-sans-v8-latin-regular.woff2",
+      rel: "preload",
+      as: "font",
+      type: "font/woff2",
+      crossorigin: "",
     },
     {
-        href: '/font/ibm-plex-sans-v8-latin-500.woff2',
-        rel: 'preload',
-        as: 'font',
-        type: 'font/woff2',
-        crossorigin: '',
+      href: "/font/ibm-plex-sans-v8-latin-500.woff2",
+      rel: "preload",
+      as: "font",
+      type: "font/woff2",
+      crossorigin: "",
     },
     {
-        href: '/font/ibm-plex-sans-v8-latin-700.woff2',
-        rel: 'preload',
-        as: 'font',
-        type: 'font/woff2',
-        crossorigin: '',
+      href: "/font/ibm-plex-sans-v8-latin-700.woff2",
+      rel: "preload",
+      as: "font",
+      type: "font/woff2",
+      crossorigin: "",
     },
     {
-        href: '/font/ibm-plex-mono-v6-latin-regular.woff2',
-        rel: 'preload',
-        as: 'font',
-        type: 'font/woff2',
-        crossorigin: '',
+      href: "/font/ibm-plex-mono-v6-latin-regular.woff2",
+      rel: "preload",
+      as: "font",
+      type: "font/woff2",
+      crossorigin: "",
     },
   ],
 
   plugins: [
     [
-      '@docusaurus/plugin-google-analytics',
+      "@docusaurus/plugin-google-analytics",
       {
-        trackingID: 'UA-64295674-3'
+        trackingID: "UA-64295674-3",
       },
     ],
     //[
@@ -106,11 +111,11 @@ const config = {
     [
       "posthog-docusaurus",
       {
-        apiKey: 'phc_hnhlqe6D2Q4IcQNrFItaqdXJAxQ8RcHkPAFAp74pubv',
-        appUrl: 'https://app.posthog.com',
+        apiKey: "phc_hnhlqe6D2Q4IcQNrFItaqdXJAxQ8RcHkPAFAp74pubv",
+        appUrl: "https://app.posthog.com",
         enableInDevelopment: false,
         opt_in_site_apps: true,
-      }
+      },
     ],
     async function myPlugin(context, options) {
       return {
@@ -133,124 +138,129 @@ const config = {
       //  disableSwitch: false,
       //  respectPrefersColorScheme: false,
       //},
-      metadata: [{name: 'keywords', content: 'netdata, monitoring, troubleshooting, servers'}],
+      metadata: [
+        {
+          name: "keywords",
+          content: "netdata, monitoring, troubleshooting, servers",
+        },
+      ],
       navbar: {
-        title: '',
+        title: "",
         logo: {
-          alt: 'Netdata',
-          src: 'img/logo.svg',
+          alt: "Netdata",
+          src: "img/logo.svg",
         },
         items: [
-            {
-                to: 'https://www.netdata.cloud/',
-                label: 'Website',
-                position: 'left',
-              },
-              {
-                to: 'https://learn.netdata.cloud/',
-                label: 'Learn',
-                position: 'left',
-              },
-              {
-                to: 'https://community.netdata.cloud/',
-                label: 'Community',
-                position: 'left',
-              },
-              {
-                to: 'https://app.netdata.cloud/',
-                label: 'App',
-                position: 'left',
-              },
-              {
-                type: 'html',
-                position: 'left',
-                value: '<div class="gcse-search"></div>',
-              },
-              {
-                to: 'https://app.netdata.cloud/',
-                label: 'Sign In',
-                position: 'right',
-              },
+          {
+            to: "https://www.netdata.cloud/",
+            label: "Website",
+            position: "left",
+          },
+          {
+            to: "https://learn.netdata.cloud/",
+            label: "Learn",
+            position: "left",
+          },
+          {
+            to: "https://community.netdata.cloud/",
+            label: "Community",
+            position: "left",
+          },
+          {
+            to: "https://app.netdata.cloud/",
+            label: "App",
+            position: "left",
+          },
+          {
+            type: "html",
+            position: "left",
+            value: '<div class="gcse-search"></div>',
+          },
+          {
+            to: "https://app.netdata.cloud/",
+            label: "Sign In",
+            position: "right",
+          },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
-            {
-                title: 'Products',
-                items: [
-                    {
-                        label: 'Agent',
-                        to: 'https://netdata.cloud/agent',
-                    },
-                    {
-                        label: 'Cloud',
-                        to: 'https://netdata.cloud/cloud',
-                    },
-                    {
-                        label: 'Integrations',
-                        to: 'https://www.netdata.cloud/integrations/',
-                    },
-                    {
-                        label: 'Status',
-                        to: 'https://status.netdata.cloud',
-                    },
-                ],
-            },
-            {
-                title: 'Resources',
-                items: [
-                    {
-                        label: 'Learn',
-                        to: '/',
-                    },
-                    {
-                        label: 'Blog',
-                        to: 'https://netdata.cloud/blog',
-                    },
-                    {
-                        label: 'GitHub',
-                        to: 'https://github.com/netdata/netdata',
-                    },
-                ],
-            },
-            {
-                title: 'Community',
-                items: [
-                    {
-                        label: 'Overview',
-                        to: 'https://www.netdata.cloud/community-overview/',
-                    },
-                    {
-                        label: 'Forums',
-                        to: 'https://community.netdata.cloud/',
-                    },
-                ],
-            },
-            {
-                title: 'Company',
-                items: [
-                    {
-                        label: 'About',
-                        to: 'https://netdata.cloud',
-                    },
-                    {
-                        label: 'News',
-                        to: 'https://www.netdata.cloud/news/',
-                    },
-                    {
-                        label: 'Careers',
-                        to: 'https://careers.netdata.cloud/',
-                    },
-                ],
-            },
+          {
+            title: "Products",
+            items: [
+              {
+                label: "Agent",
+                to: "https://netdata.cloud/agent",
+              },
+              {
+                label: "Cloud",
+                to: "https://netdata.cloud/cloud",
+              },
+              {
+                label: "Integrations",
+                to: "https://www.netdata.cloud/integrations/",
+              },
+              {
+                label: "Status",
+                to: "https://status.netdata.cloud",
+              },
+            ],
+          },
+          {
+            title: "Resources",
+            items: [
+              {
+                label: "Learn",
+                to: "/",
+              },
+              {
+                label: "Blog",
+                to: "https://netdata.cloud/blog",
+              },
+              {
+                label: "GitHub",
+                to: "https://github.com/netdata/netdata",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Overview",
+                to: "https://www.netdata.cloud/community-overview/",
+              },
+              {
+                label: "Forums",
+                to: "https://community.netdata.cloud/",
+              },
+            ],
+          },
+          {
+            title: "Company",
+            items: [
+              {
+                label: "About",
+                to: "https://netdata.cloud",
+              },
+              {
+                label: "News",
+                to: "https://www.netdata.cloud/news/",
+              },
+              {
+                label: "Careers",
+                to: "https://careers.netdata.cloud/",
+              },
+            ],
+          },
         ],
         copyright: `Copyright © ${new Date().getFullYear()} Netdata, Inc.`,
-    },
-    prism: {
-      theme: lightCodeTheme,
-      darkTheme: darkCodeTheme,
-    },
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
     }),
 };
 
